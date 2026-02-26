04805dc224eb5e00659cd231117bd3d7e2c1ea3e
AyushYadav5052 <ayush5052nov@gmail.com>
2026-02-26 20:40:00 +0530

    app.py

--- app.py
+++ app.py
@@ -18,12 +18,12 @@ st.set_page_config(
 def load_data():
     # Download similarity.pkl if not already present
     if not os.path.exists("similarity.pkl"):
-    url = "https://drive.google.com/uc?id=1AP2WXMXd4u78sOv08z4c-vk_KlXUxWXv"
-    gdown.download(url, "similarity.pkl", quiet=False)
+        url = "https://drive.google.com/uc?id=1AP2WXMXd4u78sOv08z4c-vk_KlXUxWXv"
+        gdown.download(url, "similarity.pkl", quiet=False)
 
     if not os.path.exists("movies_dict.pkl"):
-    url_movies = "https://drive.google.com/file/d/1AP2WXMXd4u78sOv08z4c-vk_KlXUxWXv/view?usp=drive_link"
-    gdown.download(url_movies, "movies_dict.pkl", quiet=False)
+        url_movies = "https://drive.google.com/file/d/1AP2WXMXd4u78sOv08z4c-vk_KlXUxWXv/view?usp=drive_link"
+        gdown.download(url_movies, "movies_dict.pkl", quiet=False)
 
     # Load movies dictionary
     with open("movies_dict.pkl", "rb") as f:
@@ -173,4 +173,5 @@ if st.session_state.get('show_results'):
             </div>
             """, unsafe_allow_html=True)
         st.write("")  # Spacer
+
 
